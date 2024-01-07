f4ccf332c71ecf72ca27e4852f7084a79422f63e
Sujal <Sujal24092004+github@gmail.com>
2024-01-07 11:39:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 let screen = document.querySelector(".screen");
 let buttons = Array.from(document.getElementsByTagName("button"));
 
+document.body.addEventListener("keydown", (e) => {
+  // console.log(e);
+  if (
+    (e.key >= 0 && e.key <= 9) ||
+    e.key == "+" ||
+    e.key == "*" ||
+    e.key == "/" ||
+    e.key == "-"
+  ) {
+    screen.innerText += e.key;
+  } else if (e.key == "=" || e.key == "Enter") {
+    try {
+      screen.innerText = screen.innerText.replace(/x/g, "*");
+      let result = eval(screen.innerText);
+      screen.innerText = result;
+    } catch (error) {
+      screen.innerText = "Invalid Expression";
+    }
+  } else if (e.key == "Backspace") {
+    let value = screen.innerText.length;
+    // console.log(value)
+    screen.innerText = screen.innerText.slice(0, value-1);
+  }
+});
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.value === "=") {
