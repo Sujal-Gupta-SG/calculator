4da66683124bbebe50322d129b5196690afcd1ee
Sujal <Sujal24092004+github@gmail.com>
2024-01-11 09:19:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,65 @@
-let screen = document.querySelector(".screen");
-let buttons = Array.from(document.getElementsByTagName("button"));
+var screen = document.querySelector("#screen");
+var btn = document.querySelectorAll(".btn");
 
-document.body.addEventListener("keydown", (e) => {
-  // console.log(e);
-  if (
-    (e.key >= 0 && e.key <= 9) ||
-    e.key == "+" ||
-    e.key == "*" ||
-    e.key == "/" ||
-    e.key == "-"
-  ) {
-    screen.innerText += e.key;
-  } else if (e.key == "=" || e.key == "Enter") {
-    try {
-      screen.innerText = screen.innerText.replace(/x/g, "*");
-      let result = eval(screen.innerText);
-      screen.innerText = result;
-    } catch (error) {
-      screen.innerText = "Invalid Expression";
-    }
-  } else if (e.key == "Backspace") {
-    let value = screen.innerText.length;
-    // console.log(value)
-    screen.innerText = screen.innerText.slice(0, value-1);
-  }
-});
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.value === "=") {
-      try {
-        screen.innerText = screen.innerText.replace(/x/g, "*");
-        let result = eval(screen.innerText);
-        screen.innerText = result;
-      } catch (error) {
-        screen.innerText = "Invalid Expression";
-      }
-    } else {
-      if (button.value === "C" || screen.innerText === "Invalid Expression") {
-        // Clear the screen when "C" is clicked or if it contains "Invalid Expression"
-        screen.innerText = "";
-      } else {
-        let value = screen.innerText;
-        if (value.length > 90) {
-          screen.innerText = value.slice(0, 90);
-        }
-        screen.innerText += button.value;
-      }
+for (item of btn) {
+  item.addEventListener("click", (e) => {
+    btntext = e.target.innerText;
+
+    if (btntext == "×") {
+      btntext = "*";
     }
+
+    if (btntext == "÷") {
+      btntext = "/";
+    }
+    screen.value += btntext;
   });
-});
+}
+
+function sin() {
+  screen.value = Math.sin(screen.value);
+}
+
+function cos() {
+  screen.value = Math.cos(screen.value);
+}
+
+function tan() {
+  screen.value = Math.tan(screen.value);
+}
+
+function pow() {
+  screen.value = Math.pow(screen.value, 2);
+}
+
+function sqrt() {
+  screen.value = Math.sqrt(screen.value, 2);
+}
+
+function log() {
+  screen.value = Math.log(screen.value);
+}
+
+function pi() {
+  screen.value = 3.14159265359;
+}
+
+function e() {
+  screen.value = 2.71828182846;
+}
+
+function fact() {
+  var i, num, f;
+  f = 1;
+  num = screen.value;
+  for (i = 1; i <= num; i++) {
+    f = f * i;
+  }
+
+  screen.value = f;
+}
+
+function backspc() {
+  screen.value = screen.value.substr(0, screen.value.length - 1);
+}
